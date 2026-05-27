e1830259aee8b8e8f2b0ef16805b042c6dfaae02
Taliban <152580783+qingtingjjjjjjj@users.noreply.github.com>
2026-05-27 11:50:21 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,20 @@
+export default {
+
+  async fetch() {
+
+    const url =
+      "https://raw.githubusercontent.com/你的用户名/iptv-auto/main/output/tv.m3u";
+
+    const res = await fetch(url);
+
+    return new Response(await res.text(), {
+
+      headers: {
+        "content-type":
+          "application/vnd.apple.mpegurl;charset=utf-8",
+
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+  }
+}
